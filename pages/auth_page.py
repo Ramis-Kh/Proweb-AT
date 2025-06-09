7cba77721890eb53c31cbdb85c4e550ba9c11d8a
Ramis-Kh <khamidov.ramis@gmail.com>
2025-06-09 12:08:58 +0500

    Добавил все файлы проекта

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -7,7 +7,7 @@ class AuthPage:
         self.driver = driver
         self.login = (By.CSS_SELECTOR, "#app > div > div > div > div.log-in__container > div > div.log-in__newContent-right > div > div.log-in__newContent-right-block-wrap > form > div.log-in__newContent-right-block-center > div > label > input")
         self.next_btn = (By.CSS_SELECTOR, "#app > div > div > div > div.log-in__container > div > div.log-in__newContent-right > div > div.log-in__newContent-right-block-wrap > form > div.log-in__newContent-right-block-bot > button")
-        self.enter_password = (By.CSS_SELECTOR, "#app > div > div > div > div.log-in__container > div > div.log-in__newContent-right > div > div.log-in__newContent-right-block-wrap > form > div.log-in__newContent-right-block-center.log-in__newContent-right-block-center-checkpass > div.log-in__newContent-right-block-center-inp > label > input")
+        self.password_input = (By.CSS_SELECTOR, "#app > div > div > div > div.log-in__container > div > div.log-in__newContent-right > div > div.log-in__newContent-right-block-wrap > form > div.log-in__newContent-right-block-center.log-in__newContent-right-block-center-checkpass > div.log-in__newContent-right-block-center-inp > label > input")
         self.dropdown = (By.CSS_SELECTOR, "#dialog > div > div > div > div.material-dialog__window-body.material-dialog__window-body_modify > div > div:nth-child(2) > div.drop-down-component__btn > div")
         self.finish_btn = (By.CSS_SELECTOR, "#dialog > div > div > div > div.material-dialog__window-body.material-dialog__window-body_modify > div > div:nth-child(2) > div.drop-down-component__content > div.sessions__item-content > button")
 
@@ -17,7 +17,7 @@ class AuthPage:
 
     def enter_password(self, password):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.presence_of_element_located(self.enter_password)).send_keys(password)
+        wait.until(EC.presence_of_element_located(self.password_input)).send_keys(password)
 
     def click_next_btn(self):
         wait = WebDriverWait(self.driver, 10)
